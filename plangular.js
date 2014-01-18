0ff62d09ae1a2ebf10074020fbb050e87d6aa2fb
Brent Jackson <jxnblk@gmail.com>
2014-01-18 12:49:07 -0500

    Callback url param

--- plangular.js
+++ plangular.js
@@ -87,7 +87,7 @@ plangular.directive('plangular', function ($document, $rootScope, $http) {
       restrict: 'A',
       scope: true,
       link: function (scope, elem, attrs) {
-        var params = { url: attrs.src, client_id: clientID }
+        var params = { url: attrs.src, client_id: clientID, callback: jsonp_callback }
         $http.jsonp('//api.soundcloud.com/resolve.json', { params: params }).success(function(data){
           // Handle playlists (i.e. sets)
           if (data.tracks) scope.playlist = data;
