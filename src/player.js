59aa899c71596c373a5e231d7ae12cc811a15805
Brent Jackson <jacksonblack@gmail.com>
2014-08-26 19:47:13 -0400

    Almost working on page tracks

--- src/player.js
+++ src/player.js
@@ -9,21 +9,33 @@ var Player = function() {
 
   var player = {};
   //player.audio = audio;
-  player.i = null;
+  player.i = 0;
   player.playing = false;
   player.tracks = [];
+  // Playlist for single tracks
+  player.playlist = [];
   player.currentTrack = {};
   player.currentTime = 0;
   player.duration = 0;
 
-  player.play = function(tracks, i) {
+  player.play = function(track, i) {
+    console.log('player.play', track, i);
     if (i == null) {
-      tracks = new Array(tracks);
+      console.log('i cant be null anymore');
+      //tracks = new Array(tracks);
+    }
+    if (track == null) {
+      console.log('aint no track');
+      var track = this.tracks[i] || this.tracks[0];
     }
     this.i = i || 0;
-    this.tracks = tracks;
-    this.currentTrack = tracks[this.i];
-    var track = this.tracks[this.i];
+    //this.tracks = tracks;
+    // consider removing this?
+    this.currentTrack = this.tracks[this.i];
+    //var track = this.tracks[this.i];
+    if (track.tracks) {
+      console.log('its a playlist so we need to handle this');
+    }
     var src = track.stream_url + '?client_id=' + plangular.clientID;
     if (src != audio.src) audio.src = src;
     audio.play();
@@ -35,19 +47,23 @@ var Player = function() {
     this.playing = false;
   };
 
-  player.playPause = function(tracks, i) {
-    if (!tracks) return false;
-    if (tracks.length && this.playing != tracks[i]) {
-      this.play(tracks, i);
-    } else if (!tracks.length && this.playing != tracks) {
-      this.play(tracks);
+  player.playPause = function(track, i) {
+    //if (!tracks) return false;
+    if (!track) console.error('no track - we have issues');
+    if (track.tracks && this.playing != track.tracks[i]) {
+      console.log('its a playlist and its not playing so play it player');
+      this.play(track, i);
+    } else if (this.playing != track) {
+      console.log('we could be playing this but you playing');
+      this.play(track, i);
     } else {
       this.pause();
     }
   };
 
   player.next = function() {
-    console.log(this);
+    console.log(this, this.playlist);
+    // Need to handle soundcloud playlists
     if (this.i < this.tracks.length - 1) {
       this.i++;
       this.play(this.tracks, this.i);
@@ -59,6 +75,11 @@ var Player = function() {
       this.i--;
       this.play(this.tracks, this.i);
     }
+  };
+
+  player.load = function(track, index) {
+    this.tracks[index] = track;
+    console.log('load', this.tracks, index);
   };
 
   player.seek = function(e) {
--- src/soundcloud.js
+++ src/soundcloud.js
@@ -1,4 +1,4 @@
-// Vue Soundcloud component
+// Vue Plangular component
 
 'use strict';
 
@@ -6,7 +6,8 @@
 var jsonp = require('jsonp');
 var player = require('./player');
 
-var Soundcloud = Vue.extend({
+var Plangular = Vue.extend({
+
   data: {
     
     player: player,
@@ -14,58 +15,55 @@ var Soundcloud = Vue.extend({
     play: function(i) {
       var url = this.$data.value;
       var track = plangular.data[url]; // Also sends full playlist for player to handle
-      var tracks;
-      if (track.tracks) tracks = track.tracks;
-      else tracks = track;
-      player.play(tracks, i);
+      var index = i || null;
+      player.play(track, index);
     },
+
     pause: function() {
       player.pause();
     },
+
     playPause: function(i) {
       var url = this.$data.value;
       var track = plangular.data[url];
-      var tracks;
-      if (track.tracks) tracks = track.tracks;
-      else tracks = track;
-      player.playPause(tracks, i);
+      var index = i || 0;
+      player.playPause(track, index);
     }
+
   },
+
   directives: {
+
     'src': function(value) {
+
       var self = this;
       self.vm.$data.value = value;
-      var preloadPlayer = function(track) {
-        if (player.tracks.length > 0) return false;
-        player.tracks = track.tracks || new Array(track);
-        player.i = 0;
-        player.currentTrack = player.tracks[player.i];
-      };
+
       var elements = document.querySelectorAll('[v-src]');
       for (var i = 0; i < elements.length; i++) {
         if (this.el == elements[i]) {
           self.vm.$data.index = i;
         }
       }
+
       var apiUrl = plangular.api + '?url=' + value + '&client_id=' + plangular.clientID;
       if (plangular.data[value]) {
         for (var key in plangular.data[value]) {
           self.vm.$data[key] = plangular.data[value][key];
         }
-        preloadPlayer(plangular.data[value]);
+        player.load(plangular.data[value], self.vm.index);
       } else {
         jsonp(apiUrl, function(error, response) {
           plangular.data[value] = response;
           for (var key in response) {
             self.vm.$data[key] = response[key];
           }
-          preloadPlayer(plangular.data[value]);
+          player.load(plangular.data[value], self.vm.index);
         });
       }
     }
   }
 });
 
-//module.exports = Vue.component('soundcloud', Soundcloud);
-Vue.component('soundcloud', Soundcloud);
+Vue.component('plangular', Plangular);
 
